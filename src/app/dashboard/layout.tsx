74377d8b4316008c312d5ab742f73c14fa19a802
Gustavo Souza <kyoro_souza@hotmail.com>
2025-09-29 09:07:55 -0300

    🎨 add sidebar as a layout structure

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { AppSidebar } from "@/components/app-sidebar";
+import { EmailHolder } from "@/components/email-holder";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+
+  function toTitleCase(str: string) {
+    return str.replace(
+      /\w\S*/g,
+      (text: string) =>
+        text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+  }
+
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              {/* <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" /> */}
+              <BreadcrumbItem>
+                <BreadcrumbPage>
+                  {toTitleCase(pathname.split("/")[1])}
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
