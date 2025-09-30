9cbd5943cc4daa0c43b8ba230931cf96cee18ebb
Gustavo Souza <kyoro_souza@hotmail.com>
2025-09-29 21:16:44 -0300

    🎨 new structure for email preview

--- src/components/email-holder.tsx
+++ src/components/email-holder.tsx
@@ -1,5 +1,4 @@
 import { Separator } from "./ui/separator";
-import MyEmail from "../../emails/my-email";
 import { ReactNode } from "react";
 
 interface EmailHolderProps {
@@ -11,7 +10,11 @@ export const EmailHolder = ({ configuration, children }: EmailHolderProps) => {
   return (
     <div className="flex flex-1">
       {configuration && (
-        <div className="flex-1 h-full p-4">{configuration}</div>
+        <div className="flex-1 h-full p-4">
+          <header className="pb-2 font-bold">Campos do email</header>
+          <Separator className="mb-4" />
+          {configuration}
+        </div>
       )}
       <Separator orientation="vertical" />
       {children && <div className="flex-2 h-full p-4">{children}</div>}
